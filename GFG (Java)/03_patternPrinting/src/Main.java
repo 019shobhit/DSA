bb4a1f754276f2293975680b533edb856a929e10
019shobhit@gmail.com <019shobhit@gmail.com>
2025-08-21 17:20:28 +0530

    add new files

---
+++ GFG (Java)/03_patternPrinting/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <n- i-1; j++) {
+                System.out.printf("  ");
+            }
+            for (int j = 0; j < (2*i)+1; j++) {
+                System.out.printf("* ");
+            }
+            System.out.println("");
+        }for (int i = 0; i < n; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.printf("  ");
+            }
+            for (int j = 0; j < (n-i-1)*2-1; j++) {
+                System.out.printf("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ GFG (Java)/Lecture_01/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        double x = 50;
+        System.out.println(x/3);
+    }
+}
